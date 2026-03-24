0d23b6b9dbb5aac4e2692c7fd4c7fcfe34ee39f5
Luis <luisvfd@gmail.com>
2026-03-23 22:30:47 -0300

    limpia build superuser

--- build.sh
+++ build.sh
@@ -4,11 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-python manage.py shell << END
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'luisvfd@gmail.com', 'clamp1378')
-END
