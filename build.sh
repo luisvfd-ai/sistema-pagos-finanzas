f1ee8c5760c6e97bbf8d578a547e707affa6a226
Luis <luisvfd@gmail.com>
2026-03-23 22:26:15 -0300

    fix superuser render

--- build.sh
+++ build.sh
@@ -5,5 +5,10 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# 👇 AGREGA ESTO TEMPORAL
-python manage.py createsuperuser --noinput || true
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'luisvfd@gmail.com', 'clamp1378')
+END
