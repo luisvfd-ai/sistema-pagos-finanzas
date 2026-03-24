00c5e6b7853b239815a1f2e001eb758d1ef931fc
Luis <luisvfd@gmail.com>
2026-03-24 15:17:50 -0300

    crear superuser automatico

--- build.sh
+++ build.sh
@@ -4,4 +4,12 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py loaddata datos_demo_limpios.json
+
+# Crear superusuario automáticamente
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'tu_correo@gmail.com', 'clave123')
+END
