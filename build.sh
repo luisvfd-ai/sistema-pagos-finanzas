dee38655b50ca5bb3444a5e3ebf5677a8f863a75
Luis <luisvfd@gmail.com>
2026-04-07 11:10:14 -0400

    limpia build script de diagnostico temporal

--- build.sh
+++ build.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "=== ARBOL REPO: pagos/static ==="
-find pagos/static -print | sort || true
-
-echo "=== ARBOL REPO: templates pagos ==="
-find pagos/templates/pagos -print | sort || true
-
 pip install -r requirements.txt
-
-echo "=== FINDSTATIC LOCAL EN BUILD ==="
-python manage.py findstatic pagos/js/reportes.js --verbosity 2 || true
-python manage.py findstatic pagos/css/reportes.css --verbosity 2 || true
-
 python manage.py collectstatic --noinput --clear
-
-echo "=== ARBOL staticfiles DESPUES DE collectstatic ==="
-find staticfiles -print | sort || true
-
-echo "=== SOLO COINCIDENCIAS reportes ==="
-find . -print | sort | grep -i reportes || true
-
 python manage.py migrate
