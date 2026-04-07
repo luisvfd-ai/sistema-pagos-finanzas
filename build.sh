4921fac36dba95b8455acb15aa0e929a12b7c300
Luis <luisvfd@gmail.com>
2026-04-07 11:55:48 -0400

    reset temporal de superuser en render

--- build.sh
+++ build.sh
@@ -4,3 +4,28 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
 python manage.py migrate
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL', '')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+if username and password:
+    user, created = User.objects.get_or_create(
+        username=username,
+        defaults={'email': email, 'is_staff': True, 'is_superuser': True}
+    )
+    if email and user.email != email:
+        user.email = email
+    user.is_staff = True
+    user.is_superuser = True
+    user.set_password(password)
+    user.save()
+    print(f'Superuser listo: {username} | creado={created}')
+else:
+    print('Faltan variables DJANGO_SUPERUSER_USERNAME o DJANGO_SUPERUSER_PASSWORD')
+"
